0c1ff3d222f20a8ab0af0f3dad887f3f1c0bb8bd
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-01-14 13:08:39 +0530

    added navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,25 +11,24 @@ function Navbar(){
 
     return(
         <div className="w-full h-14 border-b-2 border-richblack-700">
-            <nav className="flex h-full items-center justify-around">
-                <div className="flex gap-2">
-                    <TfiBarChartAlt className="w-8 h-8 text-blue-600" />
-                    <div className="text-2xl font-bold">Personal Finance Tracker</div>
+            <nav className="flex h-full w-11/12 gap-1 sm:gap-2 items-center justify-between sm:justify-around px-[1px]">
+                <div className="flex w-fit gap-[2px] sm:gap-2">
+                    <TfiBarChartAlt className="w-6 h-6 sm:w-8 sm:h-8 text-blue-600" />
+                    <div className="text-lg sm:text-2xl whitespace-nowrap font-medium sm:font-bold">Personal Finance Tracker</div>
                 </div>
                 {
                     token && 
-                    <div className="flex gap-6">
-                     <NavLink to={"/"} className="flex items-center font-semibold px-2 py-1 bg-blue-100 rounded-md gap-2">
+                    <div className="flex gap-1 w-fit sm:gap-2 md:gap-4 lg:gap-6">
+                     <NavLink to={"/"} className="flex items-center font-semibold px-1 sm:px-2 py-1 bg-blue-100 rounded-md gap-[2px] sm:gap-2">
                         <LuLayoutDashboard />
                         <span>Dashboard</span>
                     </NavLink>
-                    <NavLink to={"/expenses"} className="flex items-center font-semibold gap-2 px-2 py-1 bg-blue-100 rounded-md">
+                    <NavLink to={"/expenses"} className="flex items-center font-semibold gap-[2px] sm:gap-2 px-1 sm:px-2 py-1 bg-blue-100 rounded-md">
                         <TfiAlignCenter />
                         <span>Expenses</span>
                     </NavLink>
                 </div>
                 }
-                
             </nav>
         </div>
     );
