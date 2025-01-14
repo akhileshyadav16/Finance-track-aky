81ad36dda95ea54d3923948b809821a1e2efa183
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-01-14 14:32:51 +0530

    added nav responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,40 @@
-import { NavLink} from "react-router-dom";
-import { TfiBarChartAlt } from "react-icons/tfi";
+import { NavLink,Link } from "react-router-dom";
 import { TfiAlignCenter } from "react-icons/tfi";
 import { LuLayoutDashboard } from "react-icons/lu";
-import "./Nav.css"
+import "./Nav.css";
 
+function Navbar() {
+  const token = localStorage.getItem("token");
 
-function Navbar(){
-    const token = localStorage.getItem("token");
-
-
-    return(
-        <div className="w-full h-14 border-b-2 border-richblack-700">
-            <nav className="flex h-full w-11/12 gap-1 sm:gap-2 items-center justify-between sm:justify-around px-[1px]">
-                <div className="flex w-fit gap-[2px] sm:gap-2">
-                    <TfiBarChartAlt className="w-6 h-6 sm:w-8 sm:h-8 text-blue-600" />
-                    <div className="text-lg sm:text-2xl whitespace-nowrap font-medium sm:font-bold">Personal Finance Tracker</div>
-                </div>
-                {
-                    token && 
-                    <div className="flex gap-1 w-fit sm:gap-2 md:gap-4 lg:gap-6">
-                     <NavLink to={"/"} className="flex items-center font-semibold px-1 sm:px-2 py-1 bg-blue-100 rounded-md gap-[2px] sm:gap-2">
-                        <LuLayoutDashboard />
-                        <span>Dashboard</span>
-                    </NavLink>
-                    <NavLink to={"/expenses"} className="flex items-center font-semibold gap-[2px] sm:gap-2 px-1 sm:px-2 py-1 bg-blue-100 rounded-md">
-                        <TfiAlignCenter />
-                        <span>Expenses</span>
-                    </NavLink>
-                </div>
-                }
-            </nav>
+  return (
+    <div className="w-full h-14 border-b-2 border-richblack-700">
+      <nav className="flex h-full w-full px-2 sm:w-11/12  mx-auto gap-2 items-center justify-between sm:justify-around">
+        <div className="flex items-center gap-2">
+          <Link to={"/"} className="text-sm sm:text-xl font-serif md:text-3xl whitespace-nowrap font-bold">
+            Personal Finance Tracker
+          </Link>
         </div>
-    );
+        {token && (
+          <div className="flex gap-2 items-center">
+            <NavLink
+              to="/"
+              className="flex items-center text-sm px-1 sm:px-2 py-1 bg-blue-100 hover:bg-blue-200 rounded-md gap-2"
+            >
+              <LuLayoutDashboard className="w-5 h-5" />
+              <span>Dashboard</span>
+            </NavLink>
+            <NavLink
+              to="/expenses"
+              className="flex items-center text-sm px-1 sm:px-2 py-1 bg-blue-100 hover:bg-blue-200 rounded-md gap-2"
+            >
+              <TfiAlignCenter className="w-5 h-5" />
+              <span>Expenses</span>
+            </NavLink>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
